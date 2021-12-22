ef8915c80e64a731eb751d86e7871cac090ef89b
Tata Lee <67614323+punkzappa007@users.noreply.github.com>
2021-12-22 11:11:56 +0800

    Update twrp_CG8.mk

--- twrp_CG8.mk
+++ twrp_CG8.mk
@@ -16,7 +16,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from TECNO-CG8 device
-$(call inherit-product, device/tecno/CG8/device.mk)
+#$(call inherit-product, device/tecno/CG8/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := CG8
