ef89a0e91427ceb40e27c2d82e655bb079c81966
Tata Lee <67614323+punkzappa007@users.noreply.github.com>
2021-12-23 19:29:17 +0800

    CG8: Add TW_DEVICE_VERSION

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,7 +132,7 @@ PLATFORM_VERSION := 16.1.0
 ## TWRP-Specific configuration
 
 TW_THEME := portrait_hdpi
-TW_DEVICE_VERSION := BETA-7
+TW_DEVICE_VERSION := TecnoCamon17Pro
 TW_EXTRA_LANGUAGES := true
 TW_INCLUDE_NTFS_3G := true
 TW_HAS_MTP := true
